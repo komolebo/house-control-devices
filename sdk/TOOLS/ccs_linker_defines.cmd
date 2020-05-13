61d451f7f6dff821727256ce99262aabc5fb514b
Oleh Komarnytskyi <komolebo@gmail.com>
2020-05-13 07:28:17 +0300

    Convert SDK from staticLibrary to executable

---
+++ sdk/TOOLS/ccs_linker_defines.cmd
@@ -0,0 +1,11 @@
+/*
+** Stack Frontier Generator 1.1.0 (2020-05-11 18:37:23.357000)
+**
+** WARNING - Auto-generated file. Modifications could be lost!
+*/
+
+--define=ICALL_RAM0_START=0x20003fb0
+--define=ICALL_STACK0_START=0xa000
+--define=ICALL_STACK0_ADDR=0x118a1
+--define=NV_FLASH_PAGES=0
+
